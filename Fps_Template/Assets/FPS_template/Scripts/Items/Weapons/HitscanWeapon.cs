1bb2c980126fdbc604bf67909b6a3c947683dc02
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2024-05-30 00:07:52 +0300

    Add fire rate to Hitscan weapon

--- Fps_Template/Assets/FPS_template/Scripts/Items/Weapons/HitscanWeapon.cs
+++ Fps_Template/Assets/FPS_template/Scripts/Items/Weapons/HitscanWeapon.cs
@@ -6,23 +6,25 @@ public class HitscanWeapon : WeaponBase
 {
     [SerializeField] private LayerMask _layers;
     [SerializeField] private GameObject _decalPrefab;
-    
+    [SerializeField] private float _fireRate = 1f;
+    [SerializeField] private float _cooldownTimer = 0f;
+    [SerializeField] private bool _hasCooldown = false;
     public override void PrimaryAction()
     {
-        if (canUseAction)
+        if (canUseAction && !_hasCooldown)
         {
-            Debug.Log("shooting layers " + _layers.ToString());
+            _hasCooldown = true;
 
-            // TODO play vfx, sfx & animation 
-            // ...
+            // play vfx, sfx & animation             
+            //armsAnim.SetTrigger("Shoot");
 
             //raycast        
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity, _layers))
             {
                 Debug.Log("HitscanWeapon raycast hit " + hit.transform.gameObject.name);
-                
+
                 // check if enemy or destructable and call approritate methods (apply damage etc.)                
-                if(hit.collider.gameObject.TryGetComponent(out IHealthManager enemyHealth))
+                if (hit.collider.gameObject.TryGetComponent(out IHealthManager enemyHealth))
                 {
                     enemyHealth.TakeDamage(new DamageInfo { DamageAmount = 50 });
                 }
@@ -39,15 +41,16 @@ public class HitscanWeapon : WeaponBase
         Debug.Log("triggered secondary action");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (_hasCooldown)
+        {
+            _cooldownTimer += Time.deltaTime;
+            if (_cooldownTimer >= _fireRate)
+            {
+                _hasCooldown = false;
+                _cooldownTimer = 0f;
+            }
+        }
     }
 }
